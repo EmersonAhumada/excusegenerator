344b0e892740907f8a7cac1fc3bcb011694eecf4
EmersonAhumada <emerson.ahumada@gmail.com>
2021-08-21 17:00:32 +0000

    tarde

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -7,5 +6,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let random_who = Math.floor(Math.random() * who.length);
+
+  let action = ["ate", "peed", "crushed", "broke"];
+  let random_action = Math.floor(Math.random() * action.length);
+
+  let what = ["my homework", "the keys", "the car"];
+  let random_what = Math.floor(Math.random() * what.length);
+
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let random_when = Math.floor(Math.random() * when.length);
+
+  let result =
+    who[random_who] +
+    " " +
+    action[random_action] +
+    " " +
+    what[random_what] +
+    " " +
+    when[random_when];
+  this.document.getElementById("excuse").innerHTML = result;
 };
