5661cb307962f7ea74f6bf0f0f32e3145ab8f27c
EmersonAhumada <emerson.ahumada@gmail.com>
2021-08-25 04:20:32 +0000

    done

--- src/app.js
+++ src/app.js
@@ -4,34 +4,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  document.querySelector("button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = result();
+  });
+  let result = () => {
+    let who = ["the dog", "my granma", "his turtle", "my bird"];
+    let random_who = Math.floor(Math.random() * who.length);
 
-  let who = ["the dog", "my granma", "his turtle", "my bird"];
-  let random_who = Math.floor(Math.random() * who.length);
+    let action = ["ate", "peed", "crushed", "broke"];
+    let random_action = Math.floor(Math.random() * action.length);
 
-  let action = ["ate", "peed", "crushed", "broke"];
-  let random_action = Math.floor(Math.random() * action.length);
+    let what = ["my homework", "the keys", "the car"];
+    let random_what = Math.floor(Math.random() * what.length);
 
-  let what = ["my homework", "the keys", "the car"];
-  let random_what = Math.floor(Math.random() * what.length);
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    let random_when = Math.floor(Math.random() * when.length);
 
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-  let random_when = Math.floor(Math.random() * when.length);
-
-  let result =
-    who[random_who] +
-    " " +
-    action[random_action] +
-    " " +
-    what[random_what] +
-    " " +
-    when[random_when];
-  this.document.getElementById("excuse").innerHTML = result;
+    return (
+      who[random_who] +
+      " " +
+      action[random_action] +
+      " " +
+      what[random_what] +
+      " " +
+      when[random_when]
+    );
+  };
 };
